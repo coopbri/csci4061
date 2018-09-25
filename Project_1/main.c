e131d41d5f9110337a0cdcfc57392261403b492a
Chase Rogness <chaserogness@gmail.com>
2018-09-25 08:04:13 -0500

    Start of project 1 (#1)

---
+++ Project_1/main.c
@@ -0,0 +1,139 @@
+/* CSci4061 F2018 Assignment 1
+* login: cselabs login name (login used to submit)
+* date: mm/dd/yy
+* name: full name1, full name2, full name3 (for partner(s))
+* id: id for first name, id for second name, id for third name */
+
+// This is the main file for the code
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "util.h"
+
+/*-------------------------------------------------------HELPER FUNCTIONS PROTOTYPES---------------------------------*/
+void show_error_message(char * ExecName);
+//Write your functions prototypes here
+void show_targets(target_t targets[], int nTargetCount);
+/*-------------------------------------------------------END OF HELPER FUNCTIONS PROTOTYPES--------------------------*/
+
+
+/*-------------------------------------------------------HELPER FUNCTIONS--------------------------------------------*/
+
+//This is the function for writing an error to the stream
+//It prints the same message in all the cases
+void show_error_message(char * ExecName)
+{
+	fprintf(stderr, "Usage: %s [options] [target] : only single target is allowed.\n", ExecName);
+	fprintf(stderr, "-f FILE\t\tRead FILE as a makefile.\n");
+	fprintf(stderr, "-h\t\tPrint this message and exit.\n");
+	exit(0);
+}
+
+//Write your functions here
+
+//Phase1: Warmup phase for parsing the structure here. Do it as per the PDF (Writeup)
+void show_targets(target_t targets[], int nTargetCount)
+{
+	//Write your warmup code here
+	
+}
+
+/*-------------------------------------------------------END OF HELPER FUNCTIONS-------------------------------------*/
+
+
+/*-------------------------------------------------------MAIN PROGRAM------------------------------------------------*/
+//Main commencement
+int main(int argc, char *argv[])
+{
+  target_t targets[MAX_NODES];
+  int nTargetCount = 0;
+
+  /* Variables you'll want to use */
+  char Makefile[64] = "Makefile";
+  char TargetName[64];
+
+  /* Declarations for getopt. For better understanding of the function use the man command i.e. "man getopt" */
+  extern int optind;   		// It is the index of the next element of the argv[] that is going to be processed
+  extern char * optarg;		// It points to the option argument
+  int ch;
+  char *format = "f:h";
+  char *temp;
+
+  //Getopt function is used to access the command line arguments. However there can be arguments which may or may not need the parameters after the command
+  //Example -f <filename> needs a finename, and therefore we need to give a colon after that sort of argument
+  //Ex. f: for h there won't be any argument hence we are not going to do the same for h, hence "f:h"
+  while((ch = getopt(argc, argv, format)) != -1)
+  {
+	  switch(ch)
+	  {
+	  	  case 'f':
+	  		  temp = strdup(optarg);
+	  		  strcpy(Makefile, temp);  // here the strdup returns a string and that is later copied using the strcpy
+	  		  free(temp);	//need to manually free the pointer
+	  		  break;
+
+	  	  case 'h':
+	  	  default:
+	  		  show_error_message(argv[0]);
+	  		  exit(1);
+	  }
+
+  }
+
+  argc -= optind;
+  if(argc > 1)   //Means that we are giving more than 1 target which is not accepted
+  {
+	  show_error_message(argv[0]);
+	  return -1;   //This line is not needed
+  }
+
+  /* Init Targets */
+  memset(targets, 0, sizeof(targets));   //initialize all the nodes first, just to avoid the valgrind checks
+
+  /* Parse graph file or die, This is the main function to perform the toplogical sort and hence populate the structure */
+  if((nTargetCount = parse(Makefile, targets)) == -1)  //here the parser returns the starting address of the array of the structure. Here we gave the makefile and then it just does the parsing of the makefile and then it has created array of the nodes
+	  return -1;
+
+
+  //Phase1: Warmup-----------------------------------------------------------------------------------------------------
+  //Parse the structure elements and print them as mentioned in the Project Writeup
+  /* Comment out the following line before Phase2 */
+  show_targets(targets, nTargetCount);  
+  //End of Warmup------------------------------------------------------------------------------------------------------
+   
+  /*
+   * Set Targetname
+   * If target is not set, set it to default (first target from makefile)
+   */
+  if(argc == 1)
+	strcpy(TargetName, argv[optind]);    // here we have the given target, this acts as a method to begin the building
+  else
+  	strcpy(TargetName, targets[0].TargetName);  // default part is the first target
+
+  /*
+   * Now, the file has been parsed and the targets have been named.
+   * You'll now want to check all dependencies (whether they are
+   * available targets or files) and then execute the target that
+   * was specified on the command line, along with their dependencies,
+   * etc. Else if no target is mentioned then build the first target
+   * found in Makefile.
+   */
+	
+  //Phase2: Begins ----------------------------------------------------------------------------------------------------
+  /*Your code begins here*/
+  
+  
+  
+  
+  /*End of your code*/
+  //End of Phase2------------------------------------------------------------------------------------------------------
+
+  return 0;
+}
+/*-------------------------------------------------------END OF MAIN PROGRAM------------------------------------------*/
---
+++ Project_1/util.h
@@ -0,0 +1,100 @@
+/********************
+ * util.h
+ *
+ * You may put your utility function definitions here
+ * also your structs, if you create any
+ *********************/
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define MAX_NODES 10
+
+/*-------------------------------------------------------HELPER DEFINES----------------------------------------------*/
+#define ARG_MAX 1023
+#define TRUE 1
+#define FALSE 0
+#define ERROR -1
+#define EXISTS 1
+#define NEEDS_BUILDING 2
+#define UNFINISHED 0
+#define FINISHED 1
+/*-------------------------------------------------------END OF HELPER DEFINES---------------------------------------*/
+
+/*-------------------------------------------------------STRUCTURE DEFINITION----------------------------------------*/
+/*
+ * A Vertex in the DAG
+ */
+typedef struct target{
+  char TargetName[64]; //Target name
+  int  DependencyCount; //Number of dependencies
+  char DependencyNames[10][64]; //Names of all the dependencies
+  char Command[256]; //Command that needs to be executed for this target
+  int  Status; //Status of the target(Ready, Finished etc. based on your implementation)
+}target_t;
+/*-------------------------------------------------------END OF STRUCTURE DEFINITION---------------------------------*/
+
+
+/*-------------------------------------------------------FILE ACCESSBILITY-------------------------------------------*/
+/* Return -1 if file does not exist, otherwise return 0 */
+int does_file_exist(char *FileName);
+/*-------------------------------------------------------ENDS OF FILE ACCESSBILITY-----------------------------------*/
+
+
+/*-------------------------------------------------------TIMESTAMP COMPARATOR----------------------------------------*/
+/*
+ * Compare timestamp of File1 and File2.
+ *   Return -1 if File1 and/or File2 do not exist;
+ *   Return 0 if File1 and File2 have identical timestamp;
+ *   Return 1 if File1 is modified later than File2;
+ *   Return 2 if File1 is modified earlier than File2.
+ */
+int compare_modification_time(char *FileName1, char *FileName2);
+/*-------------------------------------------------------END OF TIMESTAMP COMPARATOR----------------------------------*/
+
+
+/*-------------------------------------------------------MAKEFILE PARSER---------------------------------------------*/
+/*
+ * Parse input Makefile.
+ *
+ * Input:
+ *   Makefile - file name
+ *   targets - a pre-allocated array holding parsed target objects.
+ * Result:
+ *    0 SUCCESS
+ *   -1 FAIL
+ */
+int parse(char *Makefile, target_t targets[]);
+/*-------------------------------------------------------END OF MAKEFILE PARSER--------------------------------------*/
+
+
+/*-------------------------------------------------------FIND NODE INDEX --------------------------------------------*/
+/*
+ * Compare TargetName with the TargetName in each target object.
+ * Return the index of the target when found, otherwise return -1.
+ *
+ * Input:
+ *   targets - targets array in main()
+ *   nTargetCount - number of target objects in Makefile,
+ *      which is returned by parse().
+ * Result:
+ *   Index of the given target
+ *   -1 NOT FOUND
+ */
+int find_target(char *TargetName, target_t targets[], int nTargetCount);
+/*-------------------------------------------------------END OF NODE INDEX-------------------------------------------*/
+
+
+/*-------------------------------------------------------MAKEARGV TOKENS---------------------------------------------*/
+/*
+ * Tokenize Command to be the argv for execvp
+ */
+int parse_into_tokens(const char *const input_string, char *tokens[], char *delim);
+/*-------------------------------------------------------END OF MAKEARGV TOKENS--------------------------------------*/
+
+
+#endif
