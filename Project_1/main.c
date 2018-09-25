696acdc2bf652f89f9db912cc7d180c95ac796c7
Brian <bcoopmn@gmail.com>
2018-09-25 11:59:46 -0500

    updated file structure

--- Project_1/main.c
+++ Project_1/main.c
@@ -1,8 +1,8 @@
 /* CSci4061 F2018 Assignment 1
-* login: cselabs login name (login used to submit)
-* date: mm/dd/yy
-* name: full name1, full name2, full name3 (for partner(s))
-* id: id for first name, id for second name, id for third name */
+* login: cselabs_login_name                           (update)
+* date : mm/dd/yy                                     (update)
+* name: Brian Cooper, Chase Rogness, Jared Erlien
+* id:     coope824,     rogne066,      erlie003   */
 
 // This is the main file for the code
 #include <errno.h>
